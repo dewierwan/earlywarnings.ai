109a03a7a96fefe43f4930241d512c6e22b41d68
Dewi Erwan <dewierwan@gmail.com>
2025-03-01 00:30:19 -0800

    adding github pages

---
+++ src/config.prod.ts
@@ -0,0 +1,4 @@
+// Production-only config with public-facing values
+export const AIRTABLE_API_KEY = 'patrag8CNItTuHXpZ.4225b0f4283a522496b98701296de20680a4cc30a7df2ac66ba006f10ff59e09';
+export const AIRTABLE_BASE_ID = 'appyyffI9hbEgePDy';
+export const AIRTABLE_TABLE_NAME = 'tblZxQZv1PmB93bKO'; 
