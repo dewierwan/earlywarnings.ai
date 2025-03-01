9ccab2773a85725f4ffe95321d5b13814150fcea
Dewi Erwan <dewierwan@gmail.com>
2025-03-01 00:50:40 -0800

    main to master

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-console.log('Base URL:', import.meta.env.BASE_URL);
-console.log('Current Path:', window.location.pathname);
-console.log('Script Path:', document.currentScript?.getAttribute('src'));
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
