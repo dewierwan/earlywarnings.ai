5b5dfe98472453461170bbc30cc84c448564b0ab
Dewi Erwan <dewierwan@gmail.com>
2025-04-12 16:07:39 +0100

    fixing posthog

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react';
+
+const POSTHOG_KEY = import.meta.env.VITE_PUBLIC_POSTHOG_KEY;
+if (!POSTHOG_KEY) {
+  throw new Error('PostHog key is not defined in environment variables');
+}
+
+// Create the client instance with explicit token
+const client = posthog.init(POSTHOG_KEY, {
+  api_host: 'https://eu.i.posthog.com'
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: 'https://eu.i.posthog.com'
-      }}
-    >
+    <PostHogProvider client={client}>
       <App />
     </PostHogProvider>
   </StrictMode>
