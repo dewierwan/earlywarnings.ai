6f28967a193f6a3466f96674f0d6b8932196a7dd
Dewi Erwan <dewierwan@gmail.com>
2025-04-12 15:45:36 +0100

    Adding posthog

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+  api_host: "https://eu.i.posthog.com",
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>
 );
